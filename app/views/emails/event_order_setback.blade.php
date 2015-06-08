020a581935d79beff9a95eda5c52e3d7dbf13ffc
Andreas Kosmowicz <kosmonowt@googlemail.com>
2015-06-08 23:27:43 +0200

    Bugfix on E-Mail Nofitication about order

--- app/views/emails/event_order_setback.blade.php
+++ app/views/emails/event_order_setback.blade.php
@@ -1,5 +1,6 @@
 @extends('emails.layouts.mail')
 @include("emails.layouts.usermail")
+@include("emails.includes.ordersList")
 
 @section("heading")
 Deine Bestellung bei {{$merchantName}} ist zurückgestellt worden.
@@ -14,7 +15,7 @@ Deine Bestellung bei {{$merchantName}} ist zurückgestellt worden.
 	@endif
 	bei
 	{{$merchantName}}</p>
-	@yield("includes.ordersList")
+	@yield("ordersList")
 	<p>wurde zurückgestellt.</p>
 	<p>Bei Fragen dazu stelle diese bitte an bestellgruppe@biokiste.org.</p>
 @stop
